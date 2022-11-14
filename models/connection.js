51d2a7c246b02d14b519b23cede6f2d26fd2bafc
Hmidux <hameir.hmida@gmail.com>
2022-11-14 16:48:03 +0100

    ...

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.STRING_CONNECTION;
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('🐶 Database connected'))
